ded44fff8ee6777e837aaf89c0c2a7a2261b5820
DavidCVz <davidcarrillovz@outlook.com>
2023-01-25 12:34:00 -0600

    Procedimientos CRUD con EF

--- ProyectoApiEF/Program.cs
+++ ProyectoApiEF/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoApiEF;
+using ProyectoApiEF.Models;
+using System.Formats.Tar;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +19,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-//Mapeo de metodos
+//Mapeo de metodos CONSULTA
 app.MapGet("/dbconexion", async ([FromServices] TareaContext dbContext) =>
 {
     // Devuelve true 
@@ -25,6 +27,7 @@ app.MapGet("/dbconexion", async ([FromServices] TareaContext dbContext) =>
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
 });
 
+// CONSULTA
 app.MapGet("/api/tareas", async ([FromServices] TareaContext dbContext) =>
 {
     //Logica
@@ -33,8 +36,53 @@ app.MapGet("/api/tareas", async ([FromServices] TareaContext dbContext) =>
     // .Tareas: Tabla de tareas en la BD
     return Results.Ok(
         //dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == ProyectoApiEF.Models.Prioridad.Bajo)); // Equivalente a una consulta anidada JOIN
-        //dbContext.Tareas.Where(p => p.PrioridadTarea == ProyectoApiEF.Models.Prioridad.Bajo)); // Equivalente a una consulta anidada JOIN
-        dbContext.Categorias.Include(p => p.Tareas).Where(p => p.Peso >= 50)); // Equivalente a SELECT en una tabla
+        dbContext.Tareas.Where(p => p.PrioridadTarea == ProyectoApiEF.Models.Prioridad.Bajo)); // Equivalente a una consulta anidada JOIN
+        //dbContext.Categorias.Include(p => p.Tareas).Where(p => p.Peso >= 50)); // Equivalente a SELECT en una tabla
+});
+
+// INSERCCION DE DATOS
+app.MapPost("/api/tareas", async ([FromServices] TareaContext dbContext, [FromBody] Tarea tarea) => //
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea); // Forma 1.
+    //await dbContext.Tareas.AddAsync(tarea); //Forma 2.
+
+    await dbContext.SaveChangesAsync(); // Confirmar los cambios y guardarlos en la BD
+
+    return Results.Ok("Inserccion de datos correctamente");
+});
+
+// ACTUALIZACION DE DATOS
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareaContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) => //
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+        return Results.Ok("Datos actualizados correctamente");
+    }
+    return Results.NotFound("Datos no encontrados");
+});
+
+// ELIMINAR DATOS
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareaContext dbContext, [FromRoute] Guid id) => //
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual != null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok("Datos eliminados correctamente");
+    }
+    return Results.NotFound("Datos no encontrados");
 });
 
 app.Run();
