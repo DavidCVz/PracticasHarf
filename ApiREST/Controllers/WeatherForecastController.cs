6908ec500e2f30c8308037c21bf7d50e2e2e8d49
DavidCVz <davidcarrillovz@outlook.com>
2023-01-26 12:57:12 -0600

    Configuracion de api

--- ApiREST/Controllers/WeatherForecastController.cs
+++ ApiREST/Controllers/WeatherForecastController.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiREST.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")] // Define la ruta en la barra de direcciones
 public class WeatherForecastController : ControllerBase
 {
+    // Arreglo que contiene
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -13,20 +14,54 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    // Crea una nueva lista del modelo WeatherForecast
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+    // Constructor
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        // Cada que se realice una peticion, se ejecuta todo el cuerpo del constructor
+        Console.WriteLine("Peticion");
+
+        /* Si la lista no contiene datos, entonces crea una coleccion Enumerable 
+            de datos aleatoreos usando la sentencia .Select, y transformandola en List*/
+        if (ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
     }
 
+    // Verbo GET
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [Route("Get/WeatherForecast")] // Define una ruta de manera explicita 
+    [Route("[action]")] /* [action] hace referencia al nombre del metodo "GetW" automaticamente, 
+                            sin importar si el nombre del metodo cambia */
+    public IEnumerable<WeatherForecast> GetW()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForecast;
+    }
+
+    // Verbo POST
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForecast.Add(weatherForecast);
+        return Ok();
+    }
+
+    // Verbo DELETE
+    // {index} refiere al parameto de entrada en la URL de la posicion del elemento a eliminar
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        ListWeatherForecast.RemoveAt(index);
+        return Ok();
     }
 }
